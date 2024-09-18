20e61093f9d68dc11d0b763b229ce928106bda2b
VictorBDev <victor.balboa.dev@gmail.com>
2024-09-17 20:44:51 -0500

    Agregar archivos de proyecto.

---
+++ sem-05/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sem_05
+{
+    internal class Cliente
+    {
+        public string idCliente { get; set; }
+        public string NombreCompañia { get; set; }
+        public string NombreContacto { get; set; }
+        public string CargoContacto { get; set; }
+        public string Ciudad { get; set; }
+    }
+}
---
+++ sem-05/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sem_05
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void botonListaCliente_Click(object sender, RoutedEventArgs e)
+        {
+            ListData();
+        }
+
+        private void ListData(string filter = "")
+        {
+            try
+            {
+                //"Data Source=DESKTOP-F970KVM;Initial Catalog=FacturaDB;User ID=usrFactura;Password=123456"
+                string cadena = "Data Source=DESKTOP-MJBK07S;Initial Catalog=Neptuno2;User ID=userNeptuno;Password=123456";
+                using SqlConnection conect = new SqlConnection(cadena);
+
+                conect.Open();
+
+                SqlCommand comand = new SqlCommand("USP_ListarClientes", conect);
+                comand.CommandType = CommandType.StoredProcedure;
+
+                List<Cliente> listaClientes = new List<Cliente>();
+                SqlDataReader reader = comand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Cliente cliente = new Cliente();
+                    cliente.idCliente = reader["idCliente"];
+                    cliente.NombreCompañia = reader["NombreCompañia"].ToString();
+                    cliente.NombreContacto = reader["NombreContacto"].ToString();
+                    cliente.CargoContacto = reader["CargoContacto"].ToString();
+                    cliente.Ciudad = reader["Ciudad"].ToString();
+                    listaClientes.Add(cliente);
+                }
+                conect.Close();
+
+                listadoClientes.DataSource = listaClientes;
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Error al cargar los productos: {e.Message}",
+                    "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
