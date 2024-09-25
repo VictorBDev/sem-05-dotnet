de818c6a0cb0e013571a1c55f1da98dc849cd44a
VictorBDev <victor.balboa.dev@gmail.com>
2024-09-24 20:41:45 -0500

    Feat/Add update data Clientes

--- sem-05/Cliente.cs
+++ sem-05/Cliente.cs
@@ -12,6 +12,13 @@ namespace sem_05
         public string NombreCompañia { get; set; }
         public string NombreContacto { get; set; }
         public string CargoContacto { get; set; }
+        public string Direccion { get; set; }
         public string Ciudad { get; set; }
+
+        public string Region { get; set; }
+        public string CodPostal { get; set; }
+        public string Pais { get; set; }
+        public string Telefono { get; set; }
+        public string Fax { get; set; }
     }
 }
--- sem-05/MainWindow.xaml.cs
+++ sem-05/MainWindow.xaml.cs
@@ -9,6 +9,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Windows.Automation;
+using System.Windows.Media.Media3D;
+using System;
 
 namespace sem_05
 {
@@ -27,12 +31,19 @@ namespace sem_05
             ListData();
         }
 
+        private void botonActualizarCliente_Click(object sender, RoutedEventArgs e)
+        {
+            ActualizarData();
+        }
+
+        //Listar datos
         private void ListData(string filter = "")
         {
             try
             {
                 //"Data Source=DESKTOP-F970KVM;Initial Catalog=FacturaDB;User ID=usrFactura;Password=123456"
-                string cadena = "Data Source=DESKTOP-MJBK07S;Initial Catalog=Neptuno2;User ID=userNeptuno;Password=123456";
+                //string cadena = "Data Source=RED\\SQLEXPRESS;Initial Catalog=Neptuno2;User ID=userNeptuno;Password=123456";
+                string cadena = "Server=RED\\SQLEXPRESS; Database=Neptuno2; Integrated Security=True; User ID=userNeptuno;Password=123456";
                 using SqlConnection conect = new SqlConnection(cadena);
 
                 conect.Open();
@@ -40,29 +51,75 @@ namespace sem_05
                 SqlCommand comand = new SqlCommand("USP_ListarClientes", conect);
                 comand.CommandType = CommandType.StoredProcedure;
 
-                List<Cliente> listaClientes = new List<Cliente>();
                 SqlDataReader reader = comand.ExecuteReader();
+                List<Cliente> listaClientes = new List<Cliente>();
 
                 while (reader.Read())
                 {
-                    Cliente cliente = new Cliente();
-                    cliente.idCliente = reader["idCliente"];
-                    cliente.NombreCompañia = reader["NombreCompañia"].ToString();
-                    cliente.NombreContacto = reader["NombreContacto"].ToString();
-                    cliente.CargoContacto = reader["CargoContacto"].ToString();
-                    cliente.Ciudad = reader["Ciudad"].ToString();
+                    Cliente cliente = new Cliente
+                    {
+                        idCliente = reader["idCliente"].ToString(),
+                        NombreCompañia = reader["NombreCompañia"].ToString(),
+                        NombreContacto = reader["NombreContacto"].ToString(),
+                        CargoContacto = reader["CargoContacto"].ToString(),
+                        Ciudad = reader["Ciudad"].ToString()
+                    };
                     listaClientes.Add(cliente);
                 }
+
+
+                listadoClientes.ItemsSource = listaClientes;
+
                 conect.Close();
 
-                listadoClientes.DataSource = listaClientes;
+                //listadoClientes.DataSource = listaClientes;
 
             }
             catch (Exception e)
             {
-                MessageBox.Show($"Error al cargar los productos: {e.Message}",
+                MessageBox.Show($"Error al cargar los clientes: {e.Message}",
                     "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
+
+        //Actualizar datos
+        private void ActualizarData(string filter = "")
+        {
+            try
+            {
+                string cadena = "Server=RED\\SQLEXPRESS; Database=Neptuno2; Integrated Security=True; User ID=userNeptuno;Password=123456";
+                using SqlConnection conect = new SqlConnection(cadena);
+
+                conect.Open();
+
+                SqlCommand comand = new SqlCommand("USP_ActualizarClientes", conect);
+                comand.CommandType = CommandType.StoredProcedure;
+
+                comand.Parameters.AddWithValue("@idCliente", txtIdCliente.Text ?? (object)DBNull.Value);
+                comand.Parameters.AddWithValue("@NombreCompañia", txtNombreCompañia.Text ?? (object)DBNull.Value);
+                comand.Parameters.AddWithValue("@NombreContacto", txtNombreContacto.Text ?? (object)DBNull.Value);
+                comand.Parameters.AddWithValue("@CargoContacto", txtCargoContacto.Text ?? (object)DBNull.Value);
+                comand.Parameters.AddWithValue("@Direccion", txtDireccion.Text ?? (object)DBNull.Value);
+                comand.Parameters.AddWithValue("@Ciudad", txtCiudad.Text ?? (object)DBNull.Value);
+                comand.Parameters.AddWithValue("@Region", txtRegion.Text ?? (object)DBNull.Value);
+                comand.Parameters.AddWithValue("@CodPostal", txtCodPostal.Text ?? (object)DBNull.Value);
+                comand.Parameters.AddWithValue("@Pais", txtPais.Text ?? (object)DBNull.Value);
+                comand.Parameters.AddWithValue("@Telefono", txtTelefono.Text ?? (object)DBNull.Value);
+                comand.Parameters.AddWithValue("@Fax", txtFax.Text ?? (object)DBNull.Value);
+
+                comand.ExecuteNonQuery();
+
+                MessageBox.Show("Cliente actualizado correctamente", "Información", MessageBoxButton.OK, MessageBoxImage.Information);
+                conect.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Error al actualizar el cliente: {e.Message}",
+                    "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        
     }
 }
